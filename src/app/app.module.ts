b5c393efaf7a18fe6f85cdb3ae8bd2ffbafd6d71
Shayane Umar <shayane.umar@gmail.com>
2018-11-25 18:30:47 +0530

    Integrate primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DynamicFormsCoreModule, DYNAMIC_VALIDATORS, ValidatorFactory } from '@ng-dynamic-forms/core';
-import { DynamicFormsMaterialUIModule } from '@ng-dynamic-forms/ui-material';
+import {
+    DynamicFormsCoreModule,
+    DYNAMIC_VALIDATORS,
+    ValidatorFactory
+} from '@ng-dynamic-forms/core';
+import { DynamicFormsPrimeNGUIModule } from '@ng-dynamic-forms/ui-primeng';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, NG_VALIDATORS, Validator, ValidationErrors } from '@angular/forms';
+import {
+    ReactiveFormsModule,
+    NG_VALIDATORS,
+    Validator,
+    ValidationErrors
+} from '@angular/forms';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { DynamicForm2Component } from './dynamic-form2/dynamic-form2.component';
 import { DynamicForm3Component } from './dynamic-form3/dynamic-form3.component';
@@ -15,32 +23,32 @@ import { DynamicFormJsonComponent } from './dynamic-form-json/dynamic-form-json.
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-
-  declarations: [
-    AppComponent,
-    DynamicFormComponent,
-    DynamicForm2Component,
-    DynamicForm3Component,
-    DynamicFormJsonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    DynamicFormsCoreModule,
-    DynamicFormsMaterialUIModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
-  providers: [
-    { provide: NG_VALIDATORS, useValue: myCustomValidator, multi: true },
-    {
-      provide: DYNAMIC_VALIDATORS,
-      useValue: new Map<string, Validator | ValidatorFactory | ValidationErrors>([
-        ['myCustomValidator', myCustomValidator]
-      ])
-    }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DynamicFormComponent,
+        DynamicForm2Component,
+        DynamicForm3Component,
+        DynamicFormJsonComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        DynamicFormsCoreModule,
+        DynamicFormsPrimeNGUIModule,
+        BrowserAnimationsModule,
+        HttpClientModule
+    ],
+    providers: [
+        { provide: NG_VALIDATORS, useValue: myCustomValidator, multi: true },
+        {
+            provide: DYNAMIC_VALIDATORS,
+            useValue: new Map<
+                string,
+                Validator | ValidatorFactory | ValidationErrors
+            >([['myCustomValidator', myCustomValidator]])
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
